36f5e1ed2ee7f8c093424006d39782bcd18ce0ce
michael orina <orinamichael4@gmail.com>
2025-06-25 12:30:17 +0000

    📅 Daily DSA commit: 2025-06-25

---
+++ 2025-06-25/search/search.go
@@ -0,0 +1,26 @@
+package search
+
+func LinearSearch(array []int, target int) (int, bool) {
+  for index, value := range array {
+    if value == target {
+      return index, true
+    }
+  }
+  return -1, false
+}
+
+func BinarySearch(array []int, target int) (int, bool) {
+  low, high := 0, len(array)
+
+  for low < high {
+    mid := low + (high - low) / 2
+
+    if array[mid] == target {
+      return mid, true
+    } else if array[mid] < target {
+      low = mid + 1
+    } else {
+      high = mid
+    }
+  }
+}
